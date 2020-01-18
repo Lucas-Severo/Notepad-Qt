d0e0714e87a6b3be810fd5571d00a24bffe5498b
Lucas Rodrigues Severo <lucas_severo@outlook.com.br>
2020-01-17 22:40:23 -0400

    open document action implemented

--- notepad.cpp
+++ notepad.cpp
@@ -1,6 +1,11 @@
 #include "notepad.h"
 #include "ui_notepad.h"
 
+#include <QFile>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QTextStream>
+
 Notepad::Notepad(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Notepad)
@@ -20,4 +25,18 @@ void Notepad::on_actionnew_triggered()
 {
     currentFile.clear();
     ui->textEdit->setText(currentFile);
+}
+
+void Notepad::on_actionopen_triggered()
+{
+    QString filename = QFileDialog::getOpenFileName(this, "Open the file");
+    QFile file(filename);
+    if(!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QMessageBox::information(this, "Information", "cannot open file: ", file.errorString());
+        return;
+    }
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
 }
